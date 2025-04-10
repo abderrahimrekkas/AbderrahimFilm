10ea77f861ec44d058f71575867d217519ba31fe
abderrahimrekkas <rekkasabderrahim8@gmail.com>
2025-04-10 13:56:29 +0200

    Initialisation du projet avec  + React + Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Trending from "./Pages/Trending";
+import Movies from "./Pages/Movies";
+import TV from "./Pages/TV";
+import Search from "./Pages/Search";
+import Error from "./Pages/Error";
+import InfoCard from "./Pages/Info";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+      <Routes>
+          <Route path="/" element={<Trending />} exact />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/tv" element={<TV />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/InfoCard/:id" element={<InfoCard />} />
+      </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
